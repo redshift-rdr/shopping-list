765e0dbaa38446b7fa4825c74ff2aa86b509ebca
Ryan Ritchie <ryan.dav.ritchie@gmail.com>
2021-08-06 16:40:43 +0100

    extended db functionality

--- classes.py
+++ classes.py
@@ -1,4 +1,6 @@
 import sqlite3, os, uuid
+from typing import Any, Dict, List
+from typing_extensions import Literal
 import config
 from datetime import datetime
 
@@ -21,7 +23,7 @@ DB Structure
 
         item_allocation
         ---------------
-        list_id | item_id | added
+        list_id | item_id | active | added
 """
 
 def connect_to_db(path : str) -> sqlite3.Connection:
@@ -38,7 +40,11 @@ def connect_to_db(path : str) -> sqlite3.Connection:
     # doesn't exist then something is wrong
     assert os.path.exists(path)
 
-    return sqlite3.connect(path)
+    # we set the row_factory on connection to get rows as dicts
+    conn = sqlite3.connect(path)
+    conn.row_factory = sqlite3.Row
+
+    return conn
 
 def get_cursor() -> sqlite3.Cursor:
     """ creates a Cursor object used to interact with the database
@@ -56,15 +62,31 @@ def db_access(function):
     
     """
     def wrapper(*args, **kwargs):
-        cursor = get_cursor()
-        function(cursor)
+        cursor : sqlite3.Cursor = get_cursor()
+        retval : Any = function(cursor, *args, *kwargs)
         cursor.connection.commit()
         cursor.connection.close()
+        return retval
     return wrapper
 
+def rows_to_dicts(rows : List[sqlite3.Row]) -> List[dict]:
+    """ takes the sqlite3.Row objects returned by cursor.fetchall() and
+        turns them into dicts
+    
+        Parameters
+        ----------
+        rows : list, required
+            A list of sqlite3.Row objects, returned from cursor.fetchall()
+            Note that the connection object used by sqlite has been set to use
+            a row_factory, which enables the conversion to dicts.
+    """
+    return [dict(row) for row in rows]
+
 @db_access
-def create_list(cursor : sqlite3.Cursor):
-    """ adds an entry to the 'lists' table
+def _create_list(cursor : sqlite3.Cursor) -> str:
+    """ creates a list by adding it to the 'lists' table
+
+        should not be called directly. use add_list
 
         Parameters
         ----------
@@ -75,3 +97,165 @@ def create_list(cursor : sqlite3.Cursor):
     """
     id = uuid.uuid4().hex
     cursor.execute("INSERT INTO 'lists' VALUES(?, ?, ?)", (id, 1, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    return id
+
+@db_access
+def add_list(cursor : sqlite3.Cursor, add_recurring_items : bool = True) -> None:
+    """ adds a new list to the application, auto adds recurring items
+
+        Parameters
+        ----------
+        cursor : sqlite3.Cursor, required
+            As long as this function is decorated with @db_access then
+            the cursor will be passed automatically, and the connection
+            closed when the function finishes.
+
+        add_recurring_items : bool, optional
+            if True, add recurring items to the list
+    """
+    list_id = _create_list()
+
+    if add_recurring_items:
+        item_list = get_recurring_items()
+        for item in item_list:
+            cursor.execute("INSERT INTO 'item_allocation' VALUES(?, ?, ?, ?)", (list_id, item['id'], datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 1))
+
+@db_access
+def remove_list(cursor : sqlite3.Cursor, list_id : str) -> None:
+    """ deletes an entry from the 'lists' table
+    
+        Parameters
+        ----------
+        cursor : sqlite3.Cursor, required
+            As long as this function is decorated with @db_access then
+            the cursor will be passed automatically, and the connection
+            closed when the function finishes.
+
+        list_id : str, required
+            The UUID of the list to be deleted
+    """
+    assert(list_id)
+
+    cursor.execute("DELETE FROM 'lists' WHERE id = ?", (list_id, ))
+
+@db_access
+def add_item(cursor : sqlite3.Cursor, list_id : str, name : str, img_path : str ='', recurring : int = 0) -> None:
+    """ adds an item to the database, and links it with a list
+    
+        An item is linked to a list using the 'item_allocation' table.
+
+        Parameters
+        ----------
+        cursor : sqlite3.Cursor, required
+            As long as this function is decorated with @db_access then
+            the cursor will be passed automatically, and the connection
+            closed when the function finishes.
+
+        list_id : str, required
+            The UUID of the list the item is being added to
+
+        name : str, required
+            The name of the item being added
+
+        img_path : str, optional
+            The path to an image to be associated with the item
+
+        recurring : int, optional
+            This controls whether the item should be added to new lists
+            when they are created
+    """
+    assert(all([list_id, name]))
+
+    # create a uuid for the item
+    item_uuid = uuid.uuid4().hex
+
+    # sql statement to add item to the items table
+    cursor.execute("INSERT INTO 'items' VALUES(?, ?, ?, ?)", (item_uuid, name, img_path, recurring))
+
+    # sql statement to link the item with a list
+    cursor.execute("INSERT INTO 'item_allocation' VALUES(?, ?, ?, ?)", (list_id, item_uuid, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 1))
+
+@db_access
+def remove_item(cursor : sqlite3.Cursor, list_id : str, item_id : str) -> None:
+    """ destroys the link between an item and a list, effectively removing the item from the list
+
+        this does not remove the item from the database, the item is still accessible in the 'items' table.
+    
+        Parameters
+        ----------
+        cursor : sqlite3.Cursor, required
+            As long as this function is decorated with @db_access then
+            the cursor will be passed automatically, and the connection
+            closed when the function finishes.
+
+        list_id : str, required
+            The UUID of the list the item is being removed from
+
+        item_id : str, required
+            The UUID of the item being removed
+    """
+    assert(all([list_id, item_id]))
+
+    cursor.execute("DELETE FROM 'item_allocation' WHERE list_id = ? AND item_id = ?", (list_id, item_id))
+
+@db_access
+def get_list(cursor : sqlite3.Cursor, list_id : str) -> List[dict]:
+    """ retrieves all items in a list 
+
+        Parameters
+        ----------
+        cursor : sqlite3.Cursor, required
+            As long as this function is decorated with @db_access then
+            the cursor will be passed automatically, and the connection
+            closed when the function finishes.
+
+        list_id : str, required
+            The UUID of the list to be retrieved
+    """
+    assert(list_id)
+
+    cursor.execute("SELECT item_id, name, img, added, active FROM 'items' LEFT OUTER JOIN 'item_allocation' ON items.id = item_allocation.item_id WHERE item_allocation.list_id = ?", (list_id, ))
+    
+    # convert rows returned by sqlite to dicts
+    return rows_to_dicts(cursor.fetchall())
+
+@db_access
+def get_items(cursor : sqlite3.Cursor, query : str = '') -> List[dict]:
+    """ retrieves all items in the database, that begins with a query string
+
+        Parameters
+        ----------
+        cursor : sqlite3.Cursor, required
+            As long as this function is decorated with @db_access then
+            the cursor will be passed automatically, and the connection
+            closed when the function finishes.
+
+        query : str, optional
+            A search string that will filter results that begin with the string
+    """
+    cursor.execute("SELECT id, name, img, recurring FROM 'items' WHERE name LIKE ?", (f'{query}%', ))
+
+    # convert rows returned by sqlite to dicts
+    return rows_to_dicts(cursor.fetchall())
+
+@db_access
+def get_recurring_items(cursor : sqlite3.Cursor) -> List[dict]:
+    """ retrieves all items in the database that have 'recurring' set to 1
+
+        Parameters
+        ----------
+        cursor : sqlite3.Cursor, required
+            As long as this function is decorated with @db_access then
+            the cursor will be passed automatically, and the connection
+            closed when the function finishes.
+    """
+    cursor.execute("SELECT id, name, img, recurring FROM 'items' WHERE recurring = 1")
+
+    # convert rows returned by sqlite to dicts
+    return rows_to_dicts(cursor.fetchall())
+
+@db_access
+def deativate_item(cursor : sqlite3.Cursor, list_id : str, item_id : str) -> None:
+    """ 
+    """
+    pass
